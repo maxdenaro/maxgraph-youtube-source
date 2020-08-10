7a435b0bebe8133cbffc5a96ba4b089c49c73d7f
maxdenaro <maxarte99@gmail.com>
2020-08-10 14:57:16 +0300

    initial commit

---
+++ PHP-фичи №2. Установка Recaptcha3 на сайт/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Captcha test</title>
+</head>
+<body>
+	<div class="container">
+		<form action="#" method="post">
+			<input type="text" name="name" placeholder="Введите имя">
+			<input type="email" name="email" placeholder="Введите email">
+			<input type="hidden" id="token" name="token">
+			<button type="submit" name="post">Отправить</button>
+		</form>
+	</div>
+	<script src="https://www.google.com/recaptcha/api.js?render=6Lec97wZAAAAAD3qvCpvfQg4UrqwKcZRv1nNrj9a"></script>
+	<script>
+	
+		document.querySelector('form').addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			let tk = '';
+
+			grecaptcha.ready(function() {
+          grecaptcha.execute('6Lec97wZAAAAAD3qvCpvfQg4UrqwKcZRv1nNrj9a', {action: 'homepage'}).then(function(token) {
+            tk = token;
+						document.getElementById('token').value = token;
+
+						const data = new URLSearchParams();
+						for (const pair of new FormData(document.querySelector('form'))) {
+								data.append(pair[0], pair[1]);
+						}
+
+						fetch('send.php', {
+							method: 'post',
+							body: data,
+						})
+						.then(response => response.json())
+						.then(result => {
+							if (result['om_score'] >= 0.5) {
+								console.log('Человек')
+								// отправка данных на почту
+							} else {
+								console.log('Бот')
+							}
+						});
+          });
+        });
+		});
+	
+	</script>
+</body>
+</html>
---
+++ PHP-фичи №2. Установка Recaptcha3 на сайт/send.php
@@ -0,0 +1,26 @@
+<?php
+
+		
+$captcha;
+if(isset($_POST['token'])){
+	$captcha=$_POST['token'];
+}
+
+$secretKey = "6Lec97wZAAAAACu1X_ipcAdJHir96UGwlZRssljS";
+$ip = $_SERVER['REMOTE_ADDR'];
+
+// post request to server
+
+$url =  'https://www.google.com/recaptcha/api/siteverify?secret=' . $secretKey .  '&response=' . $_POST['token'];
+$response = file_get_contents($url);
+$responseKeys = json_decode($response,true);
+header('Content-type: application/json');
+if($responseKeys["success"] && $responseKeys["score"] >= 0.5) {
+	echo json_encode(array('success' => 'true', 'om_score' => $responseKeys["score"], 'token' => $_POST['token']));
+} else {
+	echo json_encode(array('success' => 'false', 'om_score' => $responseKeys["score"], 'token' => $_POST['token']));
+}
+
+        
+
+ ?>
