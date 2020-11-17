a893176a5f15d15a93117e9e07188e975a4edb82
maxdenaro <maxarte99@gmail.com>
2020-11-17 14:06:10 +0300

    initial commit

---
+++ JS-плагины №10. Range слайдер для цены/js/range-slider.js
@@ -0,0 +1,37 @@
+const rangeSlider = document.getElementById('range-slider');
+
+if (rangeSlider) {
+	noUiSlider.create(rangeSlider, {
+    start: [500, 999999],
+		connect: true,
+		step: 1,
+    range: {
+			'min': [500],
+			'max': [999999]
+    }
+	});
+
+	const input0 = document.getElementById('input-0');
+	const input1 = document.getElementById('input-1');
+	const inputs = [input0, input1];
+
+	rangeSlider.noUiSlider.on('update', function(values, handle){
+		inputs[handle].value = Math.round(values[handle]);
+	});
+
+	const setRangeSlider = (i, value) => {
+		let arr = [null, null];
+		arr[i] = value;
+
+		console.log(arr);
+
+		rangeSlider.noUiSlider.set(arr);
+	};
+
+	inputs.forEach((el, index) => {
+		el.addEventListener('change', (e) => {
+			console.log(index);
+			setRangeSlider(index, e.currentTarget.value);
+		});
+	});
+}
