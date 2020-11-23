c1d8ed062528b097132f16a16e6f1c9dc277e942
maxdenaro <maxarte99@gmail.com>
2020-11-23 12:20:26 +0300

    initial commit

---
+++ Webpack/src/js/main.js
@@ -0,0 +1,11 @@
+import '../../node_modules/focus-visible/dist/focus-visible';
+
+import $ from 'jquery';
+
+import '../scss/main.scss';
+import '../index.html';
+
+
+const a = 5;
+
+$('body').css('color', 'red');
---
+++ Webpack/webpack.config.js
@@ -0,0 +1,159 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+const optimization = () => {
+  const configObj = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  };
+
+  if (isProd) {
+    configObj.minimizer = [
+      new OptimizeCssAssetWebpackPlugin(),
+      new TerserWebpackPlugin()
+    ];
+  }
+
+  return configObj;
+};
+
+const plugins = () => {
+  const basePlugins = [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: `./css/${filename('css')}`
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {from: path.resolve(__dirname, 'src/assets') , to: path.resolve(__dirname, 'app')}
+      ]
+    }),
+  ];
+
+  if (isProd) {
+    basePlugins.push(
+      new ImageminPlugin({
+        bail: false, // Ignore errors on corrupted images
+        cache: true,
+        imageminOptions: {
+          plugins: [
+            ["gifsicle", { interlaced: true }],
+            ["jpegtran", { progressive: true }],
+            ["optipng", { optimizationLevel: 5 }],
+            [
+              "svgo",
+              {
+                plugins: [
+                  {
+                    removeViewBox: false
+                  }
+                ]
+              }
+            ]
+          ]
+        }
+      })
+    )
+  }
+
+  return basePlugins;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './js/main.js',
+  output: {
+    filename: `./js/${filename('js')}`,
+    path: path.resolve(__dirname, 'app'),
+    publicPath: ''
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'app'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  optimization: optimization(),
+  plugins: plugins(),
+  devtool: isProd ? false : 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev
+            },
+          },
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: (resourcePath, context) => {
+                return path.relative(path.dirname(resourcePath), context) + '/';
+              },
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(?:|gif|png|jpg|jpeg|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: `./img/${filename('[ext]')}`
+          }
+        }],
+      },
+      {
+        test: /\.(?:|woff2)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: `./fonts/${filename('[ext]')}`
+          }
+        }],
+      }
+    ]
+  }
+};
