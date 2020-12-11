61faabfd411e353ea443d7a95735fd3be7a93252
maxdenaro <maxarte99@gmail.com>
2020-12-11 11:09:13 +0300

    initial commit

---
+++ JS-решения №12. Анимация при скролле/script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const hero = document.querySelector('.hero');
+	const header = document.querySelector('.header');
+	const scrollItems = document.querySelectorAll('.scroll-item');
+
+	const scrollAnimation = () => {
+		let windowCenter = (window.innerHeight / 2) + window.scrollY;
+		console.log(windowCenter)
+		scrollItems.forEach(el => {
+			let scrollOffset = el.offsetTop + (el.offsetHeight / 2);
+			if (windowCenter >= scrollOffset) {
+				el.classList.add('animation-class');
+			} else {
+				el.classList.remove('animation-class');
+			}
+		});
+	};
+
+	const headerFixed = () => {
+		let scrollTop = window.scrollY;
+		let heroCenter = hero.offsetHeight / 2;
+
+		if (scrollTop >= heroCenter) {
+			header.classList.add('fixed')
+			hero.style.marginTop = `${header.offsetHeight}px`;
+		} else {
+			header.classList.remove('fixed')
+			hero.style.marginTop = `0px`;
+		}
+	};
+
+	headerFixed();
+	scrollAnimation();
+	window.addEventListener('scroll', () => {
+		headerFixed();
+		scrollAnimation();
+	});
+});
