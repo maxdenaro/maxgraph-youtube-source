e5f26ed1d36dfe89f8e67afd759f471f85192051
maksim <maxgraph23@gmail.com>
2022-08-25 11:30:44 +0300

    изменил урок

--- Яндекс-карты №4. Маршрут от точки до точки/script.js
+++ Яндекс-карты №4. Маршрут от точки до точки/script.js
@@ -1,7 +1,7 @@
 ymaps.ready(function () {
 
   let myMap = new ymaps.Map('map-test', {
-    center: [59.91795236804815,30.304908500000003],
+    center: [59.91795236804815, 30.304908500000003],
     zoom: 15,
     controls: ['routePanelControl']
   });
@@ -9,20 +9,60 @@ ymaps.ready(function () {
   let control = myMap.controls.get('routePanelControl');
   let city = 'Санкт-Петербург';
 
-  control.routePanel.state.set({
-    type: 'masstransit',
-    fromEnabled: false,
-    from: `${city}, проспект Энергетиков 9`,
-    toEnabled: true,
-    to: `${city}, Невский проспект 146`,
-  });
+  // let location = ymaps.geolocation.get();
 
-  control.routePanel.options.set({
-    types: {
-      masstransit: true,
-      pedestrian: true,
-      taxi: true
-    }
-  })
-  
+  // location.then(function(res) {
+  // 	let locationText = res.geoObjects.get(0).properties.get('text');
+  // 	console.log(locationText)
+  // });
+
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  function success(pos) {
+    const crd = pos.coords;
+
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+
+
+    let reverseGeocoder = ymaps.geocode([crd.latitude, crd.longitude]);
+    let locationText = null;
+    reverseGeocoder.then(function (res) {
+      locationText = res.geoObjects.get(0).properties.get('text')
+      console.log(locationText)
+
+      control.routePanel.state.set({
+        type: 'masstransit',
+        fromEnabled: false,
+        from: locationText,
+        toEnabled: true,
+        to: `${city}, Невский проспект 146`,
+      });
+    });
+
+    console.log(locationText)
+
+    
+
+    control.routePanel.options.set({
+      types: {
+        masstransit: true,
+        pedestrian: true,
+        taxi: true
+      }
+    })
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error, options);
+
+
+
 });
